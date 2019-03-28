187907e29d2db1adb9090a2312a7ada085709071
Aric Sangchat <aric.sangchat@gmail.com>
2019-03-28 12:21:28 +0700

    added conversiontracking

--- extension/contentScript.js
+++ extension/contentScript.js
@@ -1,14 +1,6 @@
-// function callNodeServer(action) {
-//   var request = new XMLHttpRequest();
-//   request.open('POST', 'http://localhost:3000/action', true);
-//   request.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-//   request.send(JSON.stringify({action: action}));
-// }
 let saveAddress;
 
 var $newdiv1 = $( "<div class='autoFillContainer' style='position: fixed;top: 0;right: 0;'><div id='address'><p id='name'></p><p id='firstLine'></p><p id='secondLine'></p><p id='city'></p><p id='state'></p><p id='zip'></p><p id='country'></p></div><button id='pasteAddress'>Paste Address</button><button id='pasteMessage'>Paste Drop Ship Message</button></div>" );
-
-setButtons();
 
 if (window.location.host == "shoppingcart.aliexpress.com") {
   $( $newdiv1 ).appendTo("body");
@@ -27,6 +19,12 @@ if (window.location.host == "shoppingcart.aliexpress.com") {
       $(".message-text").val("Hi this is a drop shipping order. Please no invoice, no receipt, no billing info and no coupons. Thank you!");
     };
   });
+} else if (window.location.pathname == "/your/shops/me/advertising/promoted-listings") {
+  setTimeout(function(){ 
+    insertConversions();
+  }, 5000);
+} else if (window.location.pathname == "your/orders/sold") {
+  setButtons();
 }
 
 function injectAddressInPopup(addressObject) {
@@ -43,13 +41,13 @@ function injectAddressInPopup(addressObject) {
 
 function getAddress() {
   let address = {
-      name: document.getElementsByClassName("name").length >= 1 ? document.getElementsByClassName("name")[0].innerHTML : '',
-      firstLine: document.getElementsByClassName("first-line").length >= 1 ? document.getElementsByClassName("first-line")[0].innerHTML : '',
-      secondLine: document.getElementsByClassName("second-line").length >= 1 ? document.getElementsByClassName("second-line")[0].innerHTML : '',
-      city: document.getElementsByClassName("city").length >= 1 ? document.getElementsByClassName("city")[0].innerHTML : '',
-      state: document.getElementsByClassName("state").length >= 1 ? document.getElementsByClassName("state")[0].innerHTML : document.getElementsByClassName("country-name")[0].innerHTML,
-      zip: document.getElementsByClassName("zip").length >= 1 ? document.getElementsByClassName("zip")[0].innerHTML : '',
-      country: document.getElementsByClassName("country-name").length >= 1 ? document.getElementsByClassName("country-name")[0].innerHTML : ''
+      name: document.getElementsByClassName("name").length >= 1 ? document.getElementsByClassName("name")[0].innerHTML.toUpperCase() : '',
+      firstLine: document.getElementsByClassName("first-line").length >= 1 ? document.getElementsByClassName("first-line")[0].innerHTML.toUpperCase() : '',
+      secondLine: document.getElementsByClassName("second-line").length >= 1 ? document.getElementsByClassName("second-line")[0].innerHTML.toUpperCase() : '',
+      city: document.getElementsByClassName("city").length >= 1 ? document.getElementsByClassName("city")[0].innerHTML.toUpperCase() : '',
+      state: document.getElementsByClassName("state").length >= 1 ? document.getElementsByClassName("state")[0].innerHTML.toUpperCase() : document.getElementsByClassName("country-name")[0].innerHTML.toUpperCase(),
+      zip: document.getElementsByClassName("zip").length >= 1 ? document.getElementsByClassName("zip")[0].innerHTML.toUpperCase() : '',
+      country: document.getElementsByClassName("country-name").length >= 1 ? document.getElementsByClassName("country-name")[0].innerHTML.toUpperCase() : ''
   };
 
   chrome.storage.sync.set({address}, function() {
@@ -83,7 +81,7 @@ function generateProductLink() {
       console.log(product, productAttr);
 
       if (product.includes("7 Chakra, 12 Constellation")) {
-        if (productAttr == "Full Color Bracelet") {
+        if (productAttr == "Full Multi Color") {
           $("<a href='https://www.aliexpress.com/item/AMIU-12-Constellation-Lucky-Natural-Stone-Simple-Bracelet-Beads-Adjustable-Bracelet-Hollywood-Crystal-Beaded-Bohemia-Bracelets/32870248948.html?spm=a2g0s.9042311.0.0.23cb4c4dzqUQP0' target='_blank'>Order Now</a>").appendTo($(this));
         } else {
           $("<a href='https://www.aliexpress.com/item/AMIU-7-Colors-Lucky-Chakra-Natural-Stone-Simple-Bracelet-Beads-Adjustable-Bracelet-Natural-Crystal-Bohemia-Prayer/32882245473.html?spm=a2g0s.9042311.0.0.ac394c4drN8ZjI' target='_blank'>Order Now</a>").appendTo($(this));
@@ -121,7 +119,9 @@ function generateProductLink() {
           $("<a href='https://www.aliexpress.com/item/AMIU-Tibetan-Buddhist-Lucky-Charm-Tibetan-Bracelets-Bangles-For-Women-Men-Handmade-Knots-Golden-Rope-Christmas/32837148680.html?spm=2114.10010108.1000013.19.3971527fzekXJh&gps-id=pcDetailBottomMoreThisSeller&scm=1007.13339.128551.0&scm_id=1007.13339.128551.0&scm-url=1007.13339.128551.0&pvid=2d2a8d89-4ff2-40fa-8ba4-407c5bfcfec8' target='_blank'>Order Now</a>").appendTo($(this));
         } else if (productAttr == 'Brown') {
           $("<a href='https://www.aliexpress.com/item/Tibetan-Buddhist-Handmade-Lucky-Rope-Bracelet-Men-Tibetan-Buddhist-Knots-Size-Adjustable-Bracelet-For-Women/32868578644.html?spm=a2g0s.8937460.0.0.46d92e0euFJBXc' target='_blank'>Order Now</a>").appendTo($(this));
-        }
+        } else if (productAttr == 'White') {
+          $("<a href='https://www.aliexpress.com/item/AMIU-Tibetan-Buddhist-Lucky-Charm-Tibetan-Bracelets-Bangles-For-Women-Men-Handmade-Knots-Rope-Gray-Bracelet/32833777236.html?spm=a2g0s.9042311.0.0.2fe84c4dB1hFTF' target='_blank'>Order Now</a>").appendTo($(this));
+        } 
       } else if (product == 'Tibetan Lucky Rope Knots Braided Bracelet | Colorful Braided Bracelet | Yoga Bracelet Meditation Bracelet Bracelet for Women Gift for Her') {
         if (productAttr == 'Green') {
           $("<a href='https://www.aliexpress.com/item/AMIU-Tibetan-Buddhist-Lucky-Charm-Tibetan-Bracelets-Bangles-For-Women-Men-Handmade-Knots-Green-Rope-Amulet/32879660616.html?spm=a2g0s.8937460.0.0.5ac42e0ehk9CsL' target='_blank'>Order Now</a>").appendTo($(this));
@@ -142,6 +142,10 @@ function generateProductLink() {
         } else if (productAttr == 'Gold') {
           $("<a href='https://www.aliexpress.com/item/Gold-Color-Astronaut-Pendant-Necklace-Universe-Spaceman-Stainless-Steel-Chain-Necklace-for-Unisex-Couple-Outer-Space/32854959741.html?spm=2114.10010108.1000013.11.17dc155cjvwv1l&gps-id=pcDetailBottomMoreThisSeller&scm=1007.13339.128551.0&scm_id=1007.13339.128551.0&scm-url=1007.13339.128551.0&pvid=8678ad3d-c40a-489f-a1aa-2ef9e4c3439c' target='_blank'>Order Now</a>").appendTo($(this));
         }
+      } else if (product.includes('Meditating Astronaut 9')) {
+        $("<a href='https://www.aliexpress.com/item/Astronaut-Pendant-9-Planets-Solar-System-Necklace-Stars-Rock-Lava-Stones-Galaxy-Women-Novelty-Outer-Spaceman/32854515246.html?spm=a2g0s.8937460.0.0.653b2e0ePobzrF' target='_blank'>Order Now</a>").appendTo($(this));
+      } else if (product.includes('Spaceman Astronaut Key Chain')) {
+        $("<a href='https://www.aliexpress.com/item/new-arrival-high-quality-asimo-3D-key-chain-spaceman-keychain-robot-key-ring-key-holder-drop/32702161349.html?spm=a2g0s.8937460.0.0.329d2e0eJa4Bah' target='_blank'>Order Now</a>").appendTo($(this));
       } else if (product.includes('Chakra Meditation 7 Colors 3d Night Light Lamp')) {
         $("<a href='https://www.aliexpress.com/item/Acrylic-7-Color-Changing-3D-LED-nightlight-meditation-of-acrylic-bedroom-lamp-living-room-lights-Decoration/32755293418.html?spm=a2g0s.8937460.0.0.35392e0eMdpWvf' target='_blank'>Order Now</a>").appendTo($(this));
       } else if (product.includes('Meditating Astronaut Key Chain')) {
@@ -175,5 +179,33 @@ function setButtons() {
 function updateStatusClick() {
   $('#order-detail-container > div.col-group.mt-xs-4.mb-xs-2 > div:nth-child(2) > div > div.col-md-4.pr-md-0 > div > div > div > div > ul > li:nth-child(2) > span').click(function() {
     setButtons();
+  });
+}
+
+function insertConversions() {
+  let impressions;
+  let clicks;
+  let percent;
+  let totalCost;
+  let totalOrders;
+  let costPerOrder;
+  let estimatedProfit;
+
+  $( ".listing-row" ).each(function( index ) {
+    console.log(index);
+    if (index < 40 ) {
+      impressions = document.getElementsByClassName("impressions")[index].innerHTML;
+      clicks = document.getElementsByClassName("clicks")[index].innerHTML;
+      percent = clicks/impressions*100;
+
+      totalCost = parseFloat(document.getElementsByClassName("cost")[index].innerHTML.slice(1));
+      totalOrders = document.getElementsByClassName("orders")[index].innerHTML;
+      costPerOrder = totalCost/totalOrders;
+
+      estimatedProfit = ( 7 - costPerOrder ) * totalOrders;
+
+      $( "<p>"+ percent.toFixed(2) +"</p>" ).appendTo(document.getElementsByClassName("clicks")[index]);
+      $( "<p>"+ costPerOrder.toFixed(2) +"</p>" ).appendTo(document.getElementsByClassName("orders")[index]);
+      $( "<p>"+ estimatedProfit.toFixed(2) +"</p>" ).appendTo(document.getElementsByClassName("revenue")[index]);    }
   });
 }
